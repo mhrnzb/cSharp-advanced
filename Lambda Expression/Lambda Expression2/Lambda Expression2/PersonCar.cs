9db20be34c96997464c719d5eee623c7b5b5e48f
mehrnaz <mehrnazbg1381@gmail.com>
2023-08-25 19:59:42 +0330

    PersonCar class added

---
+++ Lambda Expression/Lambda Expression2/Lambda Expression2/PersonCar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda_Expression2
+{
+    internal class PersonCar
+    {
+        public int PersonID { get; set; }
+
+        public string CarName { get; set; }
+
+        public int CarModel { get; set; }
+
+    }
+}
